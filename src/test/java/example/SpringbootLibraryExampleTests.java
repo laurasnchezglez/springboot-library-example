6351718356216d98707c0d3b8ce21dd4b34baa3c
laurasnchezglez <laurasnchezglez@gmail.com>
2020-05-06 14:14:06 +0200

    change name

--- src/test/java/example/SpringbootLibraryExampleTests.java
+++ src/test/java/example/SpringbootLibraryExampleTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LibraryExampleSbApplicationTests {
+class SpringbootLibraryExampleTests {
 
 	@Test
 	void contextLoads() {
